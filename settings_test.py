b1c732cbe18a131ccdf78ae02832df441e446110
ramusus <ramusus@gmail.com>
2016-03-04 11:15:14 +0300

    updated travis, conf and settings_test

--- settings_test.py
+++ settings_test.py
@@ -1,14 +1,6 @@
 INSTALLED_APPS = (
-    'oauth_tokens',
-    'taggit',
     'vkontakte_places',
     'vkontakte_users',
     'vkontakte_groups',
 )
-
-OAUTH_TOKENS_VKONTAKTE_CLIENT_ID = 3430034
-OAUTH_TOKENS_VKONTAKTE_CLIENT_SECRET = 'b0FwzyKtO8QiQmgWQMTz'
-OAUTH_TOKENS_VKONTAKTE_SCOPE = ['ads,wall,photos,friends,stats']
-OAUTH_TOKENS_VKONTAKTE_USERNAME = '+919665223715'
-OAUTH_TOKENS_VKONTAKTE_PASSWORD = 'githubovich'
-OAUTH_TOKENS_VKONTAKTE_PHONE_END = '3715'
+SOCIAL_API_TOKENS_STORAGES = []
